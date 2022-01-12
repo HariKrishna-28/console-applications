fb6ae5ae46b6ac32a43007229a9db18f537f747e
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-01-12 20:22:27 +0530

    completed version

--- railwayReservation.java
+++ railwayReservation.java
@@ -3,6 +3,7 @@
 // to default once the application has stopped runninng.
 
 import java.util.*;
+// import java.lang.*;
 
 public class railwayReservation {
 
@@ -27,6 +28,7 @@ public class railwayReservation {
         if (user) {
             userData.put("Hario", 1234);
             userData.put("kowsi", 321);
+            userData.put("har", 123);
         }
         if (!user) {
             userData.put("hario", 12345);
@@ -150,15 +152,94 @@ public class railwayReservation {
         // }
     }
 
+    static boolean validateNewUser(HashMap<String, Integer> data, String name) {
+        return data.containsKey(name);
+    }
+
+    static List<String> getTravelDetails() {
+        // clearScreen();
+        Scanner sc = new Scanner(System.in);
+        List<String> data = new ArrayList<>();
+        String destinations;
+        while (true) {
+            try {
+                System.out.printf("Enter start and stop destination separated by space : ");
+                destinations = sc.nextLine();
+                String[] des = destinations.split(" ");
+                if (des[0].equals(des[1])) {
+                    clearScreen();
+                    System.out.println("Start and stop destinations can't be same");
+                    continue;
+                } else {
+                    data.add(des[0]);
+                    data.add(des[1]);
+                    return data;
+                }
+            } catch (Exception e) {
+                clearScreen();
+                System.out.println("Invalid inputs");
+                continue;
+            }
+        }
+
+        // return data;
+    }
+
+    static List<Boolean> generateArrangements(int range) {
+        List<Boolean> seats = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            int val = (int) Math.floor(Math.random() * (1 - 0 + 1) + 0);
+            if (val == 0) {
+                seats.add(true);
+            } else {
+                seats.add(false);
+            }
+        }
+        return seats;
+    }
+
+    static HashMap<String, List<Boolean>> generateSeats(HashMap<String, List<String>> trainData) {
+        HashMap<String, List<Boolean>> seatingArrangements = new HashMap<>();
+        for (Map.Entry<String, List<String>> entry : trainData.entrySet()) {
+            seatingArrangements.put(entry.getKey(), generateArrangements(50));
+        }
+        return seatingArrangements;
+    }
+
+    static int returnStats(List<Boolean> data) {
+        int count = 0;
+        for (boolean vals : data) {
+            if (!vals) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static HashMap<List<Integer>, List<Boolean>> modifyData(List<Boolean> data) {
+        int i = 0;
+        List<Integer> ticketDetails = new ArrayList<>();
+        HashMap<List<Integer>, List<Boolean>> newData = new HashMap<>();
+        for (int value = 0; value < data.size(); value++) {
+            if (data.get(i) == false) {
+                // System.out.print(data.get(i));
+                data.set(value, true);
+                // System.out.print(data.get(i));
+                ticketDetails.add(i + 1);
+            }
+        }
+        newData.put(ticketDetails, data);
+        return newData;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        clearScreen();
         HashMap<String, Integer> userCredentials = generateCredentials(true);
         HashMap<String, Integer> adminCredentials = generateCredentials(false);
         HashMap<String, List<String>> trainData = generateTrainData();
-        // List<String> loginData = new ArrayList<>();
+        HashMap<String, List<Boolean>> availableSeats = generateSeats(trainData);
+        clearScreen();
         System.out.println("Welcome to Railway Reservation System");
-        // System.out.println(trainData);
         while (true) {
             int choice;
             while (true) {
@@ -214,12 +295,13 @@ public class railwayReservation {
                         if (adminChoice == 1) {
                             clearScreen();
                             List<String> routeDetails = getRouteDetails();
-                            System.out.println(routeDetails);
+                            HashMap<String, List<String>> newRoute = new HashMap<>();
                             List<String> keyData = new ArrayList<>(
                                     Arrays.asList(routeDetails.get(1), routeDetails.get(2), routeDetails.get(3)));
-                            printRoutes(trainData);
                             trainData.put(routeDetails.get(0), keyData);
-                            printRoutes(trainData);
+                            newRoute.put(routeDetails.get(0), keyData);
+                            HashMap<String, List<Boolean>> newTickets = generateSeats(newRoute);
+                            availableSeats.put(routeDetails.get(0), newTickets.get(routeDetails.get(0)));
                         }
 
                         if (adminChoice == 2) {
@@ -245,19 +327,148 @@ public class railwayReservation {
 
             if (choice == 2) {
                 clearScreen();
-                List<String> data = getNames("User");
-                String userName = data.get(0);
-                int userPassword = Integer.parseInt(data.get(1));
-                if (validateUser(userCredentials, userName, userPassword)) {
-                    clearScreen();
-                    System.out.printf("Welcome User %s", userName);
-                } else {
-                    clearScreen();
-                    System.out.println("Invalid Credentials");
+                int userChoice;
+                System.out.println("User Login Portal");
+                while (true) {
+                    while (true) {
+                        System.out.println("1. Login\n2. SignUp\n3. Exit");
+                        try {
+                            userChoice = sc.nextInt();
+                            if (userChoice > 3) {
+                                clearScreen();
+                                System.out.println("Invalid choide");
+                                continue;
+                            }
+                            break;
+                        } catch (Exception e) {
+                            clearScreen();
+                            System.out.println("Invalid Input");
+                        }
+                    }
+                    if (userChoice == 3) {
+                        clearScreen();
+                        break;
+                    }
+
+                    if (userChoice == 2) {
+                        List<String> data = getNames("User");
+                        String userName = data.get(0);
+                        int userPassword = Integer.parseInt(data.get(1));
+                        if (validateNewUser(userCredentials, userName)) {
+                            clearScreen();
+                            System.out.println("User already exists. Try logging in or create a new account");
+                        } else {
+                            try {
+                                clearScreen();
+                                userCredentials.put(userName, userPassword);
+                                System.out.println("User created. Login to continue");
+                            } catch (Exception e) {
+                                clearScreen();
+                                System.out.println("Please try again later");
+                            }
+                        }
+                    }
+
+                    if (userChoice == 1) {
+                        clearScreen();
+                        List<String> data = getNames("User");
+                        String userName = data.get(0);
+                        int userPassword = Integer.parseInt(data.get(1));
+                        if (validateUser(userCredentials, userName, userPassword)) {
+                            clearScreen();
+                            int verifiedUserChoice;
+                            System.out.printf("Welcome User %s\n", userName);
+                            while (true) {
+                                while (true) {
+                                    System.out.println("1. View Trains\n2. Book Tickets\n3. Exit");
+                                    try {
+                                        verifiedUserChoice = sc.nextInt();
+                                        if (verifiedUserChoice > 3) {
+                                            clearScreen();
+                                            System.out.println("Invalid choide");
+                                            continue;
+                                        }
+                                        break;
+                                    } catch (Exception e) {
+                                        clearScreen();
+                                        System.out.println("Invalid Input");
+                                    }
+                                }
+                                if (verifiedUserChoice == 1) {
+                                    clearScreen();
+                                    printRoutes(trainData);
+                                }
+
+                                // if (verifiedUserChoice == 1) {
+                                // while (true) {
+                                // clearScreen();
+                                // List<String> travelDetails = getTravelDetails();
+                                // System.out.println(travelDetails);
+                                // break;
+                                // }
+                                // }
+
+                                if (verifiedUserChoice == 2) {
+                                    clearScreen();
+                                    String trainName, name;
+                                    int passengerCount = 10;
+                                    while (true) {
+                                        while (true) {
+                                            try {
+                                                System.out.println(trainData);
+                                                System.out.println("Enter the name of the train : ");
+                                                // name = sc.nextLine();
+                                                // System.out.println("Enter the number of passengers");
+                                                // passengerCount = sc.nextInt();
+                                                trainName = "coimbatore" + " " + "express";
+                                                // System.out.println("Enter the name of the train : ");
+                                                // name = sc.nextLine();
+                                                // trainName = name + " " + "express";
+                                                // System.out.println("Enter number of passengers : ");
+                                                // passengerCount = sc.nextInt();
+                                                break;
+                                            } catch (Exception e) {
+                                                // clearScreen();
+                                                System.out.println("Invalid entry");
+                                                continue;
+                                            }
+                                        }
+                                        if (trainData.containsKey(trainName)) {
+                                            if (passengerCount < returnStats(availableSeats.get(trainName))) {
+                                                clearScreen();
+                                                HashMap<List<Integer>, List<Boolean>> updatedData = modifyData(
+                                                        availableSeats.get(trainName));
+                                                // List<Boolean> updatedSeatingData = new ArrayList<>();
+                                                for (Map.Entry<List<Integer>, List<Boolean>> entry : updatedData
+                                                        .entrySet()) {
+                                                    // System.out.println(availableSeats.get(trainName));
+                                                    availableSeats.replace(trainName, availableSeats.get(trainName),
+                                                            entry.getValue());
+                                                    // System.out.println(availableSeats.get(trainName));
+
+                                                }
+                                            } else {
+                                                clearScreen();
+                                                System.out.printf("Only {} seats available",
+                                                        returnStats(availableSeats.get(trainName)));
+                                                break;
+                                            }
+                                        } else {
+                                            clearScreen();
+                                            System.out.println("No such train available");
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            clearScreen();
+                            System.out.println("Invalid Credentials");
+                        }
+                    }
                 }
             }
-
-            sc.close();
         }
+        sc.close();
     }
 }
