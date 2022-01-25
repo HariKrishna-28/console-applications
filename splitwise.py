ae76d487721e5c92e308b63e09dcd6b64ce50315
HariKrishna-28 <harikrishnac2002@gmail.com>
2022-01-25 21:40:09 +0530

    added new section

--- splitwise.py
+++ splitwise.py
@@ -32,8 +32,62 @@ def authenticate_user(name, password, database):
     return False
 
 
+def get_price():
+    while True:
+        try:
+            print("Wallet Recharge")
+            n = int(input("Enter the price : "))
+            return n
+        except ValueError:
+            clear_screen()
+            print("Invalid input")
+            continue
+
+
+def create_contact_group(name, database):
+    database[name] = [name]
+    return database
+
+
+def modify_contacts(name, database, user_details, add=True):
+    updated_database = database
+    while True:
+        try:
+            r = int(input("Enter the number of users : "))
+            for i in range(r):
+                print("Enter exit if you want to exit this window")
+                user_name = input("Enter the id of the user : ")
+                if user_name.lower() == "exit":
+                    return updated_database
+                else:
+                    if user_name not in user_details:
+                        clear_screen()
+                        print("No such user")
+                    else:
+                        data = updated_database[name]
+                        if add:
+                            data.append(user_name)
+                        if not add:
+                            try:
+                                data.remove(user_name)
+                            except ValueError:
+                                clear_screen()
+                                print("No such user")
+            return updated_database
+        except ValueError:
+            clear_screen()
+            print("Invalid input")
+            continue
+
+
 user_credentials = {"hari@gmail.com": "123",
-                    "krishna@gmail.com": "321", "kowsik@gmail.com": "1212", "dina@gmail.com": "1232"}
+                    "krishna@gmail.com": "321", "kowsik@gmail.com": "1212", "dina@gmail.com": "1232", "hk@gmail.com": "123"}
+
+wallet_amount = {"hari@gmail.com": 35000, "krishna@gmail.com": 20000,
+                 "kowsik@gmail.com": 0, "dina@gmail.com": 500000, "hk@gmail.com": 5000}
+
+contact_groups = {
+    "hk@gmail.com": ["hk@gmail.com", "hari@gmail.com", "kowsik@gmail.com"], }
 
 # Driver code
 clear_screen()
@@ -58,6 +112,8 @@ while True:
             name, password = get_names(type="new user")
             if name not in user_credentials:
                 user_credentials[name] = password
+                wallet_amount[name] = 0
+                contact_groups = create_contact_group(name, contact_groups)
                 clear_screen()
                 print("Successfully created an account. Login to continue")
                 break
@@ -88,9 +144,56 @@ while True:
                         print("Invalid entry")
                         continue
 
+                if user_choice == 2:
+                    clear_screen()
+                    price = get_price()
+                    print(wallet_amount)
+                    wallet_amount[name] = wallet_amount[name]+price
+                    print(wallet_amount)
+
                 if user_choice == 8:
                     clear_screen()
                     break
+
+                # if user_choice == 3:
+                #     clear_screen()
+                #     if name not in contact_groups:
+                #         contact_groups = create_contact_group(
+                #             name, contact_groups)
+                #         print("Successfully created a contact group")
+                #     else:
+                #         clear_screen()
+                #         print("Contact group already exists. Try adding contacts")
+                #     continue
+
+                if user_choice == 4:
+                    clear_screen()
+                    # if name not in contact_groups:
+                    #     contact_groups = create_contact_group(
+                    #         name, contact_groups)
+                    prev_length = len(contact_groups[name])
+                    contact_groups = modify_contacts(
+                        name, contact_groups, user_credentials, add=True)
+                    print("Added {} contacts".format(
+                        prev_length-len(contact_groups[name])))
+
+                if user_choice == 5:
+                    clear_screen()
+                    # if name not in contact_groups:
+                    #     contact_groups = create_contact_group(
+                    #         name, contact_groups)
+                    print(contact_groups[name])
+                    if len(contact_groups[name]) == 1:
+                        clear_screen()
+                        print("Empty contacts")
+                        continue
+                    else:
+                        clear_screen()
+                        prev_length = len(contact_groups[name])
+                        contact_groups = modify_contacts(
+                            name, contact_groups, user_credentials, add=False)
+                        print("Removed {} contacts".format(
+                            prev_length-len(contact_groups[name])))
 
         else:
             print("Invalid credentials")
